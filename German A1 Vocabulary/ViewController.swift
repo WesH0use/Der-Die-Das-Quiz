7c12ca7e5fc5dfbde4344d1f0aa81a7fcac1b3ee
Wes House <wesevan7@gmail.com>
2019-06-30 11:17:03 -0400

    Initial Commit

---
+++ German A1 Vocabulary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  German A1 Vocabulary
+//
+//  Created by Wesley House on 6/28/19.
+//  Copyright © 2019 Wesley House. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
